772c542cb885c1e09e09144beba0aa08f4b4248d
evgenia-taipova <evgenia.taipova@yandex.ru>
2024-11-19 22:46:56 +0100

    add validation input via state and blur

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -5,17 +5,35 @@ export default function Login() {
     email: "",
     password: "",
   });
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("User email: " + enteredValues.email)
-    console.log("User password: " + enteredValues.password)
-
+    console.log("User email: " + enteredValues.email);
+    console.log("User password: " + enteredValues.password);
+    setEnteredValues({ email: "", password: "" });
   }
+
   function handleInputChange(identifier, value) {
     setEnteredValues((presValues) => ({
       ...presValues,
       [identifier]: value,
+    }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
     }));
   }
 
@@ -30,9 +48,13 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
